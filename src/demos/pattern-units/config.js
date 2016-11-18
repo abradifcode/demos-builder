25825ddfcf6520453901b1234c82df53dedad7d1
yokselzok <yokselzok@gmail.com>
2016-11-19 00:10:57 +0300

    Add patternUnits demo

---
+++ src/demos/pattern-units/config.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var $ = tinyLib;
+
+var config = {
+  demoContentLayout: 'both-ranges',
+  targetElem: $.get( '#pattern' ),
+
+  inputs: {
+    ranges: {
+      targetElem: $.get('#pattern'),
+      units: '%',
+
+      attrs: {
+        type: 'range',
+        min: 0,
+        max: 100,
+        step: 1,
+        value: 50,
+      },
+
+      list: [
+        {
+          id: 'hor',
+          valName: 'width'
+        },
+        {
+          id: 'vert',
+          valName: 'height'
+        }
+      ]
+    },
+
+    buttons: {
+      prop: 'patternUnits',
+      list: [ 'objectBoundingBox', 'userSpaceOnUse' ],
+      default: 'userSpaceOnUse',
+      current: null,
+      currentClass: 'button--current',
+      // switchUnits: true
+    }
+  }
+};
+
+demosBuilder.create();
