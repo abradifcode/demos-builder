15efb926fb5ead4d250416d3c66dcaa1793be56c
yokselzok <yokselzok@gmail.com>
2016-11-19 00:12:20 +0300

    Add patternUnits in px demo

---
+++ src/demos/pattern-units-px/config.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var $ = tinyLib;
+
+var config = {
+  demoContentLayout: 'both-ranges',
+  targetElem: $.get( '#pattern' ),
+
+  inputs: {
+    ranges: {
+      targetElem: $.get('#pattern'),
+      isTied: true,
+      // units: '%',
+
+      attrs: {
+        type: 'range',
+        min: 0,
+        max: 300,
+        step: 1,
+        value: 50,
+      },
+
+      list: [
+        {
+          id: 'hor',
+          valName: 'width'
+        },
+        {
+          id: 'vert',
+          valName: 'height'
+        }
+      ]
+    },
+
+    buttons: {
+      prop: 'patternUnits',
+      list: [ 'objectBoundingBox', 'userSpaceOnUse' ],
+      default: 'userSpaceOnUse',
+      current: null,
+      currentClass: 'button--current',
+      // switchUnits: true
+    }
+  }
+};
+
+demosBuilder.create();
