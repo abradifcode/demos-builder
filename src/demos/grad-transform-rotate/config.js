b330459c31cb8574ed37362621cf9841a01c5d4c
yokselzok <yokselzok@gmail.com>
2016-11-18 23:57:17 +0300

    Add demo for gradient transform

---
+++ src/demos/grad-transform-rotate/config.js
@@ -0,0 +1,53 @@
+// Required
+// 1. tinyLib:
+// http://codepen.io/yoksel/pen/KMgMvw
+// 2. demosBuilder:
+// http://codepen.io/yoksel/pen/JKRKyy
+
+console.clear();
+
+var $ = tinyLib;
+
+var config = {
+  demoContentLayout: 'top-range',
+  targetElem: $.get( '#grad' ),
+
+  inputs: {
+    ranges: {
+      // units: '',
+
+      attrs: {
+        type: 'range',
+        min: 0,
+        max: 360,
+        step: 1,
+        value: 180,
+      },
+
+      list: [
+        {
+          id: 'hor',
+          valName: 'gradientTransform',
+          transform: {
+            name: 'rotate',
+            origin: {
+              'objectBoundingBox': ['.5','.5'],
+              'userSpaceOnUse': ['250','150']
+            }
+          }
+        }
+      ]
+    },
+
+    buttons: {
+      prop: 'gradientUnits',
+      list: [ 'objectBoundingBox', 'userSpaceOnUse' ],
+      default: 'userSpaceOnUse',
+      current: null,
+      currentClass: 'button--current',
+      switchUnits: true
+    }
+  }
+};
+
+demosBuilder.create();
